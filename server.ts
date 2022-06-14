64734fd9e3a0d51daa5e6561e166499f0619c04b
Shameel Fazul <shameelfazul@ShameelMac.local>
2022-06-14 18:11:42 +0530

    scheduled to check every hour

--- server.ts
+++ server.ts
@@ -12,8 +12,7 @@ dotenv.config();
 const device = devices["Desktop Chrome"];
 const T = new Twit({ consumer_key: process.env.consumer_key, consumer_secret: process.env.consumer_secret, access_token: process.env.access_token, access_token_secret: process.env.access_token_secret });
 
-//cron.schedule(`* * */1 * *`, () => main());
-main();
+cron.schedule(`* * */1 * *`, () => main());
 
 async function main() {
     try {
